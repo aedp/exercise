c521668c22aed52e4e0bc0e6743e3d23b3b8d891
aedp <1493158609@qq.com>
2024-06-24 21:13:32 +0800

    first commit

---
+++ hello.c
@@ -0,0 +1,11 @@
+/*
+ * @Author: Zeng Zhiwei 
+ * @Date: 2024-06-24 20:02:46 
+ * @Last Modified by:   Zeng Zhiwei 
+ * @Last Modified time: 2024-06-24 20:02:46 
+ */
+#include <stdio.h>
+void main()
+{
+    printf("hello");
+}
---
+++ hello1.c
@@ -0,0 +1,15 @@
+/*
+ * @Author: Zeng Zhiwei 
+ * @Date: 2024-06-24 20:03:12 
+ * @Last Modified by:   Zeng Zhiwei 
+ * @Last Modified time: 2024-06-24 20:03:12 
+ */
+#include <stdio.h>
+char a;
+short b;
+int c;
+void main(){
+    printf("hello world\n");
+    printf("%d %d %d\n",sizeof(a),sizeof(b),sizeof(c));
+    
+}
